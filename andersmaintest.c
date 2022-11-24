088d53908b16bbd7c5d5f3c8d9057437079ccbb6
Soya <Axewu_Reitzel@hotmail.com>
2022-11-24 21:19:28 +0100

    Test Anders program
    Jeg leger lidt rundt med filer og gnuplot

---
+++ andersmaintest.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_POINTS 6
+#define NUM_COMMANDS 12
+
+typedef struct // Koekken,stue,bad,værelse,soveværelse.
+{
+  float bad;
+  float koekken;
+  float sovevaerelse;
+  float stue;
+  float vaerelse;
+} rum;
+
+// array af structs til dataen.
+rum data[100];
+
+
+int average(rum data[], int linjer);
+
+int main(void)
+{
+  // opretter variablen fil, for at kunne læse data.
+  FILE *file;
+
+  file = fopen("data.csv", "r");
+
+  // Kommer med besked hvis filen ikke åbner.
+  if (file == NULL)
+  {
+    printf("error opening file\n");
+    return 1;
+  }
+  
+  // bliver brugt til at tjekke om filen bliver læst ordenligt
+  int read = 0;
+  // variablen skal bruges til at kigge på hvor mange linjer der er i programmet, som kan bruges i koden længere nede.
+  int linjer = 0;
+
+  do // læs alle linjer af data og sæt dem ind i array af structs, som der passer til.
+  {
+    read = fscanf(file,
+                  "%f ,%f ,%f ,%f ,%f \n",
+                  &data[linjer].bad,
+                  &data[linjer].koekken,
+                  &data[linjer].sovevaerelse,
+                  &data[linjer].stue,
+                  &data[linjer].vaerelse);
+
+    // hvis den kan læse alle 5 værdier, så betyder det at der er en linje.
+    if (read == 5)
+      linjer++;
+
+    // hvis den ikke kan læse 5 værdier, og den ikke er nået til slutningen af filen, så print en fejlbesked.
+    if (read != 5 && !feof(file))
+    {
+      printf("File format is incorrect.\n");
+      return 1;
+    }
+
+  } while (!feof(file));
+
+  fclose(file);
+
+  printf("\n %d lines read.\n", linjer);
+
+  // print hver linje data ud i konsolen.
+  for (int i = 0; i < linjer; i++)
+  {
+    printf("%lf %lf %lf %lf %lf",
+           data[i].bad,
+           data[i].koekken,
+           data[i].sovevaerelse,
+           data[i].stue,
+           data[i].vaerelse);
+    printf("\n");
+  }
+
+
+  average(data, linjer);
+  return 0;
+}
+
+int average(rum data[], int linjer){
+  float sum_bad = 0;
+  float sum_koekken = 0;
+  float sum_sovevaerelse = 0;
+  float sum_stue = 0;
+  float sum_vaerelse = 0;
+
+  // udregning af sum, for at få gennemsnit.
+  for (int i = 0; i < linjer; i++)
+  {
+    sum_bad = sum_bad + data[i].bad;
+    sum_koekken = sum_koekken + data[i].koekken;
+    sum_sovevaerelse = sum_sovevaerelse + data[i].sovevaerelse;
+    sum_stue = sum_stue + data[i].stue;
+    sum_vaerelse = sum_vaerelse + data[i].vaerelse;
+  }
+
+  float gns_bad_24 = 0;
+  float gns_koekken_24 = 0;
+  float gns_sovevaerelse_24 = 0;
+  float gns_stue_24 = 0;
+  float gns_vaerelse_24 = 0;
+
+  float samlet_24 = (sum_bad+sum_koekken+sum_sovevaerelse+sum_stue+sum_vaerelse);
+
+  // divider summen med antal elementer, som er linjer, for  at få gennemsnit.
+  gns_bad_24 = sum_bad / linjer;
+  gns_koekken_24 = sum_koekken / linjer;
+  gns_sovevaerelse_24 = sum_sovevaerelse / linjer;
+  gns_stue_24 = sum_stue / linjer;
+  gns_vaerelse_24 = sum_vaerelse / linjer;
+
+/*
+  printf("\nGennemsnit kW paa bad brugt over %d antal timer er %lfkW\n", linjer, gns_bad_24);
+  printf("\nGennemsnit kW paa koekken brugt over %d antal timer er %lfkW\n", linjer, gns_koekken_24);
+  printf("\nGennemsnit kW paa sovevaerelse brugt over %d antal timer er %lfkW\n", linjer, gns_sovevaerelse_24);
+  printf("\nGennemsnit kW paa stue brugt over %d antal timer er %lfkW\n", linjer, gns_stue_24);
+  printf("\nGennemsnit kW paa vaerelset brugt over %d antal timer er %lfkW\n", linjer, gns_vaerelse_24);
+*/
+  
+  char * commandsForGnuplot[] = {
+    "set title \"Dit stroemforbrug\"", 
+    "set xlabel \"Rum\"", 
+    "set ylabel \"Gennemsnit kWh over 48 timer\"", 
+    "set boxwidth 0.8", 
+    "set style fill   solid 1.00 border lt -1",
+    "set yrange [0:20]", 
+    "set xtics   (\"Samlet\" 1.00000, \"Bad\" 2.00000, \"Koekken\" 3.0000, \"Sovevaerelse\" 4.0000, \"Stue\" 5.0000, \"Vaerelse\" 6.0000)", 
+    "set xtic rotate by -45 scale 0",
+    "unset border", 
+    "set tics scale 0", 
+    "set xzeroaxis", 
+    "plot 'data.temp' smooth freq w boxes"
+    };
+  
+  double xvals[NUM_POINTS] = {1.0 ,2.0, 3.0, 4.0, 5.0, 6.0};
+  double yvals[NUM_POINTS] = {samlet_24, sum_bad, sum_koekken, sum_sovevaerelse, sum_stue, sum_vaerelse};
+  FILE * temp = fopen("data.temp", "w");
+  /*Opens an interface that one can use to send commands as if they were typing into the
+    *     gnuplot command line.  "The -persistent" keeps the plot open even after your
+    *     C program terminates.
+    */
+  
+  FILE * gnuplotPipe = popen ("gnuplot -persistent", "w");
+  int i;
+
+  for (i=0; i < NUM_POINTS; i++){
+  fprintf(temp, "%lf %lf \n", xvals[i], yvals[i]); //Write the data to a temporary file
+  }
+
+  for (i=0; i < NUM_COMMANDS; i++){
+  fprintf(gnuplotPipe, "%s \n", commandsForGnuplot[i]); //Send commands to gnuplot one by one.
+  }
+  return 0;
+
+}
+
+
