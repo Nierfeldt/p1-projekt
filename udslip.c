7b3f8aa62df284e73387a60846c7a9775c725a56
Nierfeldt <oliver@nierfeldt.dk>
2022-11-25 10:32:35 +0100

    added graphs with gnuplot to udslip

--- udslip.c
+++ udslip.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_POINTS 24
+#define NUM_COMMANDS 6
+#define NUM_LINES 24
 
 
 typedef struct
@@ -19,6 +24,9 @@ typedef struct
 
 carbon emission[300];
 
+
+void skriv_alle_udslip_til_fil(FILE *fil, int xvals[], carbon ba[], int laengde);
+void skriv_udslip_for_time_til_fil(FILE *fil, carbon b);
 
 int main(void)
 {
@@ -157,8 +165,75 @@ int main(void)
   printf("\nTidspunktet hvor der bliver udslippet mindst co2 paa dag 11 er kl: %d og udslippet er : %lf\n",location_11,emission[location_11].dag_11);
   printf("\nTidspunktet hvor der bliver udslippet mindst co2 paa dag 12 er kl: %d og udslippet er : %lf\n",location_12,emission[location_12].dag_12);
 
+
+  /*forslag til gnuplot flere grafer:  */
+  
+  char * commandsForGnuplot[] = {"set title \"Noget med CO2\"", "set xlabel \"Tid paa dagen\"",
+  "set ylabel \"CO2-udslip i hvad\"", "set xrange [1:24]", "set yrange [0:300]", 
+  "plot 'udslip.temp' using 1:2 w l title 'dag 1', \
+     '' using 1:3 w l title 'dag 2', \
+     '' using 1:4 w l title 'dag 3', \
+     '' using 1:5 w l title 'dag 4', \
+     '' using 1:6 w l title 'dag 5', \
+     '' using 1:7 w l title 'dag 6', \
+     '' using 1:8 w l title 'dag 7', \
+     '' using 1:9 w l title 'dag 8', \
+     '' using 1:10 w l title 'dag 9', \
+     '' using 1:11 w l title 'dag 10', \
+     '' using 1:12 w l title 'dag 11', \
+     '' using 1:13 w l title 'dag 12' \
+     "};
+  int xvals[NUM_POINTS] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
+  //FILE *temp = tmpfile();
+  FILE *temp = fopen("udslip.temp", "w");
+  /*Opens an interface that one can use to send commands as if they were typing into the
+    *     gnuplot command line.  "The -persistent" keeps the plot open even after your
+    *     C program terminates.
+    */
+  FILE * gnuplotPipe = popen ("gnuplot -persistent", "w");
+  int i, days;
+  
+  skriv_alle_udslip_til_fil(temp, xvals, emission, NUM_LINES);
+  /* nu er punkterne skrevet ind i en fil som:
+   x y1 y2 y3 y4 y5 y6 y7 y8 y9 y10 y11 y12     altså: tid dag1 dag2 da3 dag4 dag5 dag6 dag7 dag8 dag9 dag10 dag11 dag12
+   */
+
+  for (i=0; i < NUM_COMMANDS; i++){
+    fprintf(gnuplotPipe, "%s \n", commandsForGnuplot[i]); //Send commands to gnuplot one by one.
+  }
+  fclose(temp);
+  
   return 0;
 }
 
 
 
+
+void skriv_alle_udslip_til_fil(FILE *fil, int xvals[], carbon ba[], int laengde){
+  int i;
+  for(i = 0; i < laengde; i++){
+    fprintf(fil, "%d ", xvals[i]);
+    skriv_udslip_for_time_til_fil(fil, ba[i]);
+    fprintf(fil, "\n");
+  }
+}
+
+void skriv_udslip_for_time_til_fil(FILE *fil, carbon b){
+  fprintf(fil, "%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf", 
+    b.dag_1, b.dag_2, b.dag_3, b.dag_4, b.dag_5, b.dag_6, 
+    b.dag_7, b.dag_8, b.dag_9, b.dag_10, b.dag_11, b.dag_12
+  );
+}
+
+/*
+# Set the output file type
+set terminal postscript eps enhanced color solid colortext 9
+# Set the output file name
+set output 'multiple_plots.eps'
+
+# Now plot the data with lines and points
+plot 'data.txt' using 1:2 w l title 'y1', \
+     '' using 1:3 w l title 'y2', \
+     '' using 1:4 w l title 'y3', \
+     '' using 1:4 w l title 'y4'
+     */
